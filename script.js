657e781c23d4ca4d75d33123edea71cd91ae4b0f
AlbrizioLuca <122476062+AlbrizioLuca@users.noreply.github.com>
2023-03-24 21:46:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ snake[0] = {
 
 let direction = null;
 
+// Gérer la direction du snake et éviter les "marches arrières"
 document.addEventListener('keydown', (event) => {
     if (event.key == 'ArrowLeft' && direction != 'Right'){
         direction = 'Left';
@@ -51,46 +52,59 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-// const collision = ( newHead , snake) => {
-//     let body = snake.slice(1);
-//         for (let i=0; i < body.length; i++) {
-//             if( newHead.x === body[i].x &&
-//                 newHead.y === body[i].y);
-//             return true
-//         }
-//         return false
-//     }
+// Définir s'il y a une collision du snake sur lui-même
+const collision = ( newHead , snake) => {
+    // Récupérer le corps du snake sans sa tête, qui est le premier index du tableau
+    let body = snake.slice(1);
+    // Si la position de la tête correspond à la position d'une partie du snake, alors collision
+    for (let i = 0; i < body.length; i++) {
+        if ( newHead.x === body[i].x && newHead.y === body[i].y) {
+            return true;
+        }
+    }
+    // Sinon pas de collision et le jeu continue
+    return false;
+}
 
+// Créer le canvas
 function draw(){
-    context.drawImage(background, 0, 0)
-    context.drawImage(souris, mouse.x, mouse.y)
-    for (let i = 0; i < snake.length; i++) {
+    context.drawImage(background, 0, 0) // le background du jeu
+    context.drawImage(souris, mouse.x, mouse.y) // la souris
+    // Dessiner la tete et le corps du serpent avec deux couleurs distinctes
+    for (let i = 0; i < snake.length; i++) { 
+        // Tete à l'index 0 du tableau
         if(i === 0){
-            // context.src = 'src/snake.png'
             context.fillStyle = 'white'
+        // Restant du corps
         } else {
             context.fillStyle = 'lime'
         }
-        context.fillRect(snake[i].x, snake[i].y,unit,unit),
-        context.strokeRect(snake[i].x, snake[i].y,unit,unit),
+        // Dessiner le contour du serpent
+        context.fillRect(snake[i].x, snake[i].y,unit,unit), // remplit le rectangle 
+        context.strokeRect(snake[i].x, snake[i].y,unit,unit), // dessine le contour du rectangle
         context.strokeStyle = 'brown'
     }
+    // Définir la position de la tete du serpent
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;
 
+    // Initier le déplacement en fonction de la direction
     if (direction == 'Left') snakeX -= unit
     if (direction == 'Up') snakeY -= unit
     if (direction == 'Right') snakeX += unit
     if (direction == 'Down') snakeY += unit
 
-    if(snakeX == mouse.x && snakeY == mouse.y){
+    // Si la position de la tête est la même que celle de la souris
+    if (snakeX == mouse.x && snakeY == mouse.y){
+        // Incrementer le score
         score++
+        // Repositionner aléatoirement la souris
         mouse = {
             x:Math.floor(Math.random()*19)*unit,
             y:Math.floor(Math.random()*19)*unit
         }
-    }
-    else {
+    } else {
+        // Sinon créer le déplacement en retirant le dernier item du snake (qui sera récupéré avec l'ajout de la nouvelle position)
         snake.pop()
     }
 
@@ -99,18 +113,22 @@ function draw(){
         y: snakeY
     }
 
+    // Ajouter la nouvelle position au début du snake pour le faire avancer
     snake.unshift(newHead)
 
-    if( snakeX <= -unit || snakeX >= canvas.width ||
-        snakeY <= -unit || snakeY >= canvas.height
-        // collision(newHead, snake)
-        ){
+    // Si on sort du cadre, ou s'il y a une collision
+    if ( snakeX <= -unit || snakeX >= canvas.width || snakeY <= -unit || snakeY >= canvas.height || collision(newHead, snake)) {
+        // Fin du jeu, on affiche "Game Over"
         clearInterval(play);
         button.style.display = 'block'
-        } 
-        scoreSpan.textContent = score;
+    }
+    
+    // Afficher le nouveau score
+    scoreSpan.textContent = score;
 }
+// Relancer une partie, en cliquant sur le bouton "Game Over" pour refresh la page 
 const clickButton = () => {
     window.location.reload()
 }
-let play = setInterval(draw, 150)
+// Définir la vitesse du jeu en ms
+let play = setInterval(draw, 150);
